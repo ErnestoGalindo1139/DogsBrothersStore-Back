0440a370b96a59feebeb23aa69b1d7d8bffe741a
Juan Carlos Rivas <juan_carlosr223@hotmail.com>
2024-01-25 00:34:10 -0700

    Nombre de la categoría en obtener todos los productos

--- src/database/querys.js
+++ src/database/querys.js
@@ -1,5 +1,5 @@
 export const queries = {
-    getAllProducts: "SELECT * FROM productos",
+    getAllProducts: "SELECT *, CASE WHEN id_categoria = 1 THEN 'Celulares' WHEN id_categoria = 2 THEN 'Laptops' WHEN id_categoria = 3 THEN 'Accesorios' WHEN id_categoria = 4 THEN 'Computadoras' WHEN id_categoria = 5 THEN 'Componentes' END AS categoria_producto FROM productos",
     addNewProduct: "INSERT INTO productos (nombre_producto, descripcion_producto, precio_producto, cantidad_producto, url_producto, id_categoria) VALUES (@nombre_producto, @descripcion_producto, @precio_producto, @cantidad_producto, @url_producto, @id_categoria)",
     getProductById: "SELECT * FROM productos WHERE id_producto = @id_producto",
     deleteProduct: "DELETE FROM productos WHERE id_producto = @id_producto",
